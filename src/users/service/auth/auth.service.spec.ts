ade19906768f225f4662e4b84e194c11b6064706
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-10-05 23:59:58 -0300

    auth service tests

--- src/users/service/auth/auth.service.spec.ts
+++ src/users/service/auth/auth.service.spec.ts
@@ -3,13 +3,13 @@ import { UsersService } from '../users/users.service';
 import { createMock } from '@golevelup/ts-jest';
 import { HashingService } from './hashing.service';
 import { AuthService } from './auth.service';
-import { User } from '../../../orm/users/user.entity';
 import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { faker } from '@faker-js/faker';
 
 describe('AuthService', () => {
   let authService: AuthService;
   let fakeUsersService: UsersService;
+  let fakeHashingService: HashingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,13 +22,10 @@ describe('AuthService', () => {
 
     authService = module.get<AuthService>(AuthService);
     fakeUsersService = module.get<UsersService>(UsersService);
+    fakeHashingService = module.get<HashingService>(HashingService);
   });
 
-  it('should be defined', () => {
-    expect(authService).toBeDefined();
-  });
-
-  it('throws an error if user signs up with email that is in use', async () => {
+  it('SignUp ---> throws an error if user signs up with email that is in use', async () => {
     const randomEmail = faker.internet.email();
     const randomPassword = faker.internet.password();
     fakeUsersService.find = jest.fn().mockReturnValue(randomEmail);
@@ -38,7 +35,7 @@ describe('AuthService', () => {
     ).rejects.toThrow(BadRequestException);
   });
 
-  it('should not throw an error if no user is found', async () => {
+  it('SignUp ---> should not throw an error if no user is found', async () => {
     const randomEmail = faker.internet.email();
     const randomPassword = faker.internet.password();
     fakeUsersService.find = jest.fn().mockReturnValue('');
@@ -46,5 +43,35 @@ describe('AuthService', () => {
     await expect(
       authService.signUp(randomEmail, randomPassword),
     ).resolves.not.toThrowError(BadRequestException);
+  });
+
+  it('SignIn ---> should  throw an error if no user is found', async () => {
+    const randomEmail = faker.internet.email();
+    const randomPassword = faker.internet.password();
+    fakeUsersService.find = jest
+      .fn()
+      .mockReturnValue(new Promise((resolve) => resolve([])));
+
+    await expect(
+      authService.signIn(randomEmail, randomPassword),
+    ).rejects.toThrowError(NotFoundException);
+  });
+
+  it('SignIn ---> should  throw an error if no user is found', async () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    fakeUsersService.find = jest
+      .fn()
+      .mockReturnValue(
+        new Promise((resolve) => resolve([{ email, password }])),
+      );
+
+    fakeHashingService.validateHash = jest
+      .fn()
+      .mockReturnValue(new Promise((resolve) => resolve(false)));
+
+    await expect(authService.signIn(email, password)).rejects.toThrowError(
+      BadRequestException,
+    );
   });
 });
